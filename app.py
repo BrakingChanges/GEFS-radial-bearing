51081598475a0b5c29c1e4730e57258053807699
Tech-Helper503 <helpertech83@gmail.com>
2024-04-05 04:46:26 +0300

    Got the python house in control
    *Achievements:
    * Formatted the file to be useful in documentation
    * Eliminated the use of globals with the flask app context
    
    TODO:
    * Implement Aircraft track storage to enable users to see the previous track of the aircraft if the app closes

--- app.py
+++ app.py
@@ -1,25 +1,28 @@
-from flask import Flask, request
-from flask_cors import CORS
-from json import load
-from requests import get
+
+""" Module allowing traversal of airport files"""
 from os import walk
-from os import system
+
+from json import load
+from flask import Flask, request, g
+from flask_cors import CORS
+from requests import get
 
 
 master_approach = {}
 files = []
+filenamees = []
 
 for dirpath, dirnames, filenamees in walk('data'):
     files.extend(filenamees)
     break
 
 for file_ in filenamees:
-    with open(f'./data/{file_}','r') as f:
+    with open(f'./data/{file_}','r', encoding='utf-8') as f:
         approaches = load(f)
         master_approach[file_] = approaches
 
 
-res  = get('https://www.simbrief.com/api/xml.fetcher.php?userid=519024&json=1')
+res  = get('https://www.simbrief.com/api/xml.fetcher.php?userid=519024&json=1', timeout=10)
 data = res.json()
 
 
@@ -37,17 +40,15 @@ app = Flask(__name__)
 CORS(app)
 
 data = {
-    "lat": None,
-    "lon": None,
-    "heading": None,
-    "altitude": None
+
 }
 
 @app.route("/data", methods=['POST'])
 def recieve_loc_data():
-    global data
-    
-    data = request.get_json(force=True)
+    """ Save data from GEOFS Client"""
+
+    data_ = request.get_json(force=True)
+    g.data = data_
 
     return {
         'message': 'Success'
@@ -55,21 +56,36 @@ def recieve_loc_data():
 
 @app.route("/data-get")
 def send_loc_data():
-    return data
+    """ Send all the location data to the viewer interface"""
+    if 'data' in g:
+        data_ = g.data
+    else:
+        data_ = {
+            "lat": None,
+            "lon": None,
+            "heading": None,
+            "altitude": None
+        }   
+
+    return data_
 
 
 @app.route("/route-data")
 def recieve_route():
+    """ Sends route data to interface """
     return route_data
 
 @app.route("/get-approach")
 def recieve_approach():
+    """Get an approach for a specific ICAO code"""
     airport_name = request.args.get('icao')
     approach_name = request.args.get('app_name')
     return master_approach[airport_name[approach_name]]
 
 @app.route("/get-all-approaches")
 def recieve_approaches():
+    """ Get all the approaches of a specific ICAO code"""
+
     airport_name = request.args.get('icao')
 
     return master_approach[airport_name]
